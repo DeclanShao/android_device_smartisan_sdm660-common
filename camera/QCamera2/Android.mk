8e0e08805b40632ed2b3e85f5b908823a48a7ed0
Vandana Jain <vandja@codeaurora.org>
2020-04-02 16:36:59 +0530

    sdm660-common: QCamera2: update libion header path to use internal wrapper
    Update libion header path to use internal wrapper to compile this
    modules.
    
    Change-Id: Id247a7d464411b3c3697d5e1a410de14ce2689b3

--- camera/QCamera2/Android.mk
+++ camera/QCamera2/Android.mk
@@ -106,9 +106,14 @@ LOCAL_C_INCLUDES := \
         $(TARGET_OUT_HEADERS)/mm-camera-lib/cp/prebuilt
 
 ifneq ($(TARGET_KERNEL_VERSION),$(filter $(TARGET_KERNEL_VERSION),3.18 4.4 4.9))
-LOCAL_C_INCLUDES += \
-        system/core/libion/kernel-headers \
-        system/core/libion/include
+  ifneq ($(LIBION_HEADER_PATH_WRAPPER), )
+    include $(LIBION_HEADER_PATH_WRAPPER)
+    LOCAL_C_INCLUDES += $(LIBION_HEADER_PATHS)
+  else
+    LOCAL_C_INCLUDES += \
+            system/core/libion/kernel-headers \
+            system/core/libion/include
+  endif
 endif
 
 #HAL 1.0 Include paths
