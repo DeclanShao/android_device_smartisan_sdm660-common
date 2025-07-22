4ada2802ca64988a9ab6f261d6ad77f32745fd53
DeclanShao <74823397+DeclanShao@users.noreply.github.com>
2025-07-22 13:12:36 +0000

    sdm660-common: Disable camera hal1

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -119,6 +119,7 @@ QCOM_BT_USE_SMD_TTY := true
 
 # Camera
 BOARD_QTI_CAMERA_32BIT_ONLY := true
+TARGET_SUPPORT_HAL1 := false
 TARGET_LD_SHIM_LIBS += /vendor/lib/hw/camera.sdm660.so|/vendor/lib/libshim_camera_sdm660.so
 TARGET_LD_SHIM_LIBS += /vendor/lib/libmmcamera_ppeiscore.so|/vendor/lib/libshim_camera.so
 
