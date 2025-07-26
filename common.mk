d65cc20af69b17e0b59a68ae92ad8e3084b7619b
DeclanShao <74823397+DeclanShao@users.noreply.github.com>
2025-07-26 13:03:32 +0000

    sdm660-common: Drop ims shim

--- common.mk
+++ common.mk
@@ -354,7 +354,6 @@ PRODUCT_PACKAGES += \
 
 # RIL
 PRODUCT_PACKAGES += \
-    libshim_ims \
     android.hardware.radio@1.2 \
     android.hardware.radio.config@1.0 \
     android.hardware.radio.deprecated@1.0 \
