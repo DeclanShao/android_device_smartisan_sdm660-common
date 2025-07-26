02ec83a7ca6c75d86fc4b24b3bbb2bf428f43841
DeclanShao <74823397+DeclanShao@users.noreply.github.com>
2025-07-26 13:13:38 +0000

    sdm660-common: Set kernel config

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -62,7 +62,8 @@ BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_HEADER_ARCH := arm64
 TARGET_KERNEL_SOURCE := kernel/smartisan/sdm660
-TARGET_KERNEL_CONFIG := smartisan-perf_defconfig
+TARGET_KERNEL_CONFIG := vendor/osborn_defconfig
+TARGET_KERNEL_VERSION := 4.19
 
 # HAX: SELinux Permissive - Remove ASAP
 BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
